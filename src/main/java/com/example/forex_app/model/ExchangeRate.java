9463e15370d26cac22a50d498a2ccc0b8d9a730a
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-22 12:31:38 +0200

    ExchangeRate class is created
    The ExchangeRate entity will store the current exchange rates between two currencies.

---
+++ src/main/java/com/example/forex_app/model/ExchangeRate.java
@@ -0,0 +1,52 @@
+package com.example.forex_app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class ExchangeRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fromCurrency;
+    private String toCurrency;
+    private BigDecimal rate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public void setFromCurrency(String fromCurrency) {
+        this.fromCurrency = fromCurrency;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public void setToCurrency(String toCurrency) {
+        this.toCurrency = toCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
