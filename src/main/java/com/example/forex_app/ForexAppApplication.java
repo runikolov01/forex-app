b18bd670918b876135f45aaecbb63ae5c63c09ee
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-22 12:21:05 +0200

    Spring Initializr files are added

---
+++ src/main/java/com/example/forex_app/ForexAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.forex_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forex_app/ForexAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forex_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
