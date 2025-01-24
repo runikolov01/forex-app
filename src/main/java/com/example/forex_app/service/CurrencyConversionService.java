7678cc05816c8a721602bad331154aeba52d6c15
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-24 21:41:51 +0200

    CurrencyConversionService class is updated

--- src/main/java/com/example/forex_app/service/CurrencyConversionService.java
+++ src/main/java/com/example/forex_app/service/CurrencyConversionService.java
@@ -3,13 +3,14 @@ package com.example.forex_app.service;
 import com.example.forex_app.model.CurrencyConversion;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 public interface CurrencyConversionService {
     CurrencyConversion convertCurrency(BigDecimal amount, String fromCurrency, String toCurrency);
 
     CurrencyConversion getConversionByTransactionId(String transactionId);
 
-    List<CurrencyConversion> getConversionsByDate(LocalDateTime startDate, LocalDateTime endDate);
+    List<Map<String, Object>> getConversionHistory(String transactionId, String transactionDate);
+
 }
