f42c0d28b117fa8439b35da6c20b4e76e397dee7
Rumen Nikolov <rumenhco2001@abv.bg>
2025-01-24 22:21:48 +0200

    @Repository annotation is added

--- src/main/java/com/example/forex_app/repository/CurrencyConversionRepository.java
+++ src/main/java/com/example/forex_app/repository/CurrencyConversionRepository.java
@@ -3,12 +3,18 @@ package com.example.forex_app.repository;
 import com.example.forex_app.model.CurrencyConversion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CurrencyConversionRepository extends JpaRepository<CurrencyConversion, Long> {
     Optional<CurrencyConversion> findByTransactionId(String transactionId);
 
-    List<CurrencyConversion> findByTransactionDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+    Page<CurrencyConversion> findByTransactionDateBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
 }
--- src/main/java/com/example/forex_app/repository/ExchangeRateRepository.java
+++ src/main/java/com/example/forex_app/repository/ExchangeRateRepository.java
@@ -2,9 +2,11 @@ package com.example.forex_app.repository;
 
 import com.example.forex_app.model.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
     Optional<ExchangeRate> findByFromCurrencyAndToCurrency(String fromCurrency, String toCurrency);
 }
